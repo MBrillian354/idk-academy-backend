422bc9186d0e0ad2c0df5e02ce920946c51c7c40
MBrillian354 <brillian354@gmail.com>
2025-07-11 13:14:34 +0700

    Imrproved CORS handling

--- server.js
+++ server.js
@@ -18,15 +18,16 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
 // Set Access-Control-Allow-Origin header for all responses
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 app.use(express.json());
