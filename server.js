ff445ce13db76d32c0f494e65afd25a975c587fa
MBrillian354 <brillian354@gmail.com>
2025-07-11 10:53:04 +0700

    add inbound connection monitoring

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const session = require('express-session');
 
 const app = express();
 connectDB();
+// Log inbound connections
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // Middleware
 app.use(cors({
